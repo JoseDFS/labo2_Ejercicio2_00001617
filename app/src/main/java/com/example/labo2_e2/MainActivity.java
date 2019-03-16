c6eb3a394b84517e8dc2abcd6a08df02a82dbdd8
Jose Segura <00001617@uca.edu.sv>
2019-03-16 11:20:10 -0600

    Aplicacion funcionando al clickear una imagen cambia aleatoriamente a otra

--- app/src/main/java/com/example/labo2_e2/MainActivity.java
+++ app/src/main/java/com/example/labo2_e2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.labo2_e2;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,8 +12,9 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     private ImageView img1, img2, img3 , img4, img5, img6, img7, img8, img9;
+    private Drawable d1,d2,d3,d4,d5,d6,d7,d8,d9;
     private Random rand = new Random();
-    private int n;
+    private int n=3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,17 +30,112 @@ public class MainActivity extends AppCompatActivity {
         img8 = findViewById(R.id.image_8);
         img9 = findViewById(R.id.image_9);
 
+        d1= img1.getDrawable();
+        d2= img2.getDrawable();
+        d3= img3.getDrawable();
+        d4= img4.getDrawable();
+        d5= img5.getDrawable();
+        d6= img6.getDrawable();
+        d7= img7.getDrawable();
+        d8= img8.getDrawable();
+        d9= img9.getDrawable();
+
+
         img1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                n= 0;
-                switch (n){
-                    case 0:
-                        img1.setImageDrawable(img2.getDrawable());
-                }
-
+                changeImg(img1);
             }
         });
+        img2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeImg(img2);
+            }
+        });
+        img3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeImg(img3);
+            }
+        });
+        img3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeImg(img3);
+            }
+        });
+        img4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeImg(img4);
+            }
+        });
+        img5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeImg(img5);
+            }
+        });
+        img6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeImg(img6);
+            }
+        });
+        img7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeImg(img7);
+            }
+        });
+        img8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeImg(img8);
+            }
+        });
+        img9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeImg(img9);
+            }
+        });
+
+    }
+
+    public void changeImg(ImageView img){
+        n= rand.nextInt(9);
+        System.out.print(n);
+        switch (n){
+            case 0:
+                img.setImageDrawable(d1);
+                break;
+            case 1:
+                img.setImageDrawable(d2);
+                break;
+            case 2:
+                img.setImageDrawable(d3);
+                break;
+            case 3:
+                img.setImageDrawable(d4);
+                break;
+            case 4:
+                img.setImageDrawable(d5);
+                break;
+            case 5:
+                img.setImageDrawable(d6);
+                break;
+            case 6:
+                img.setImageDrawable(d7);
+                break;
+            case 7:
+                img.setImageDrawable(d8);
+                break;
+            case 8:
+                img.setImageDrawable(d9);
+                break;
+        }
 
     }
 
